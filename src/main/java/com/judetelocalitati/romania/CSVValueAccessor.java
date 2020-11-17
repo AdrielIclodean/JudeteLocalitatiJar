de088123dbf6449859e44b0c8b456fb6c8a7f9aa
Adriel <githubPass25>
2020-11-18 00:56:41 +0200

    Add new method

--- src/main/java/com/judetelocalitati/romania/CSVValueAccessor.java
+++ src/main/java/com/judetelocalitati/romania/CSVValueAccessor.java
@@ -5,10 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CSVValueAccessor {
 
@@ -40,6 +37,47 @@ public class CSVValueAccessor {
         return citiesWithCounty;
     }
 
+    /**
+     * Get all the counties for the given city
+     *
+     * @param city - City from Romania
+     * @return - List with all the counties
+     */
+    public static List<String> getCountiesForCity(String city) {
+
+        city = StringUtils.stripAccents(city);
+        CSVReader reader;
+        List<String> citiesWithCounty = new LinkedList<>();
+        String[] line;
+        try {
+            reader = new CSVReader(new InputStreamReader(CSVValueAccessor.class.getClassLoader().getResourceAsStream("judete-localitati.csv")));
+            boolean found = false;
+            while ((line = reader.readNext()) != null) {
+                String lineCity = line[1];
+                String county = line[2];
+
+                if (lineCity.equals(city)) {
+                    found = true;
+                }
+
+                if (lineCity.equals(city) && found) {
+                    citiesWithCounty.add(county);
+                    continue;
+                }
+
+                if (found) {
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return citiesWithCounty;
+    }
+
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
@@ -48,9 +86,11 @@ public class CSVValueAccessor {
         System.out.println(citiesWithCounties.get(StringUtils.stripAccents("Ilfov")));
         //"cod_judet","judet","localitate","resedinta_judet","oras"
 
-        System.out.print("CIties time ");
+        System.out.print("Cities time ");
         System.out.println(System.currentTimeMillis() - start);
 
         System.out.println(citiesWithCounties.keySet());
+
+        System.out.println(getCountiesForCity("Arad"));
     }
 }
